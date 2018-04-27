3fb5947cdbc554770df68af9ef8aad72f46e4675
Darya Ignatova <darya.ignatova.99@mail.ru>
2018-04-27 18:41:11 +0400

    Start page without styles

--- DI.Reminder.Web/App_Start/RouteConfig.cs
+++ DI.Reminder.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DI.Reminder.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Prompt", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
