f643ac2f53ca9ba181ed7b600cbf5b7aaacfd53a
Darya Ignatova <darya.ignatova.99@mail.ru>
2018-05-05 23:07:25 +0400

    PromptDetails + Service.Start

---
+++ AppService/App_Code/Advertising.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для Advertising
+/// </summary>
+public class Advertising:IAdvertising
+{
+
+    public string GetData(int value)
+    {
+        return null;
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    {
+        return null;
+    }
+}
---
+++ AppService/App_Code/IAdvertising.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для IAdvertising
+/// </summary>
+/// [ServiceContract]
+public interface IAdvertising
+{
+    
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        
+}
