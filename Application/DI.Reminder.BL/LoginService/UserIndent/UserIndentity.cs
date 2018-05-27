e6174f40a65596f1b40bed524d8ef2ffe3c8b4ff
Darya Ignatova <darya.ignatova.99@mail.ru>
2018-05-27 18:11:57 +0400

    UserIndentity and UserProvider were added

---
+++ Application/DI.Reminder.BL/LoginService/UserIndent/UserIndentity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using DI.Reminder.Common.LoginModels;
+using DI.Reminder.Data.AccountDatabase;
+
+namespace DI.Reminder.BL.LoginService.UserIndent
+{
+    public class UserIdentity:IIdentity
+    {
+        public Account _account { get; set; }
+        public string AuthenticationType
+        {
+            get
+            {
+                return typeof(Account).ToString();
+            }
+        }
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return _account != null;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                if (_account != null)
+                    return _account.Login;
+                return "anonym";
+            }
+
+        }
+        
+        public void Init(string login, IAccountRepository accountRepository)
+        {
+            if(!string.IsNullOrEmpty(login))
+            {
+                _account = accountRepository.GetAccount(login);
+            }
+        }
+    }
+}
---
+++ Application/DI.Reminder.BL/LoginService/UserProv/UserProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using DI.Reminder.BL.LoginService.UserIndent;
+using DI.Reminder.Data.AccountDatabase;
+using DI.Reminder.Data.RolesRepository;
+
+namespace DI.Reminder.BL.LoginService.UserProv
+{
+    class UserProvider : IPrincipal
+    {
+        private IRoleRepository _roleRepository;
+        private IAccountRepository _accountRepository;
+        public UserProvider(IRoleRepository roleRepository, string name, IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+            userIndentity = new UserIndentity();
+            userIndentity.Init(name,_accountRepository);
+            _roleRepository = roleRepository;
+        }
+        private UserIndentity userIndentity { get; set; }
+        public IIdentity Identity
+        {
+            get
+            {
+                return userIndentity;
+            }
+        }
+        public bool IsInRole(string role)
+        {
+            if (userIndentity._account == null || string.IsNullOrWhiteSpace(role))
+            {
+                return false;
+            }
+            var UserRoles = _roleRepository.GetRoleList(userIndentity._account.ID);
+            var rolesArray = role.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var _role in rolesArray)
+            {
+                var hasRole = UserRoles.Any(p => string.Compare(p.Name, role, true) == 0);
+                if (hasRole)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
