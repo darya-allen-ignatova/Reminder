e7ea2023c29f8d7b5f9dfdd7ad77452fde49f5d4
Darya Ignatova <darya.ignatova.99@mail.ru>
2018-06-06 18:38:56 +0400

    Add and delete category view added

--- Application/DI.Reminder.Web/Controllers/CategoryController.cs
+++ Application/DI.Reminder.Web/Controllers/CategoryController.cs
@@ -5,8 +5,8 @@ using System.Web.Mvc;
 
 namespace DI.Reminder.Web.Controllers
 {
-    [Admin]
-    [Editor]
+    //[Admin]
+    //[Editor]
     public class CategoryController : Controller
     {
         ICategories _categoriesStorage;
