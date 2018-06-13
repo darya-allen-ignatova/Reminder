17029cd5833a06eb508ff8ca491c8252dc144d77
Darya Ignatova <darya.ignatova.99@mail.ru>
2018-06-13 14:07:02 +0400

    Prompt model was modified with validation attributes

--- Application/DI.Reminder.Common/PromptModel/Prompt.cs
+++ Application/DI.Reminder.Common/PromptModel/Prompt.cs
@@ -7,14 +7,19 @@ namespace DI.Reminder.Common.PromptModel
 {
     public class Prompt
     {
+        [Key]
         public int ID { get; set; }
         public int userID { get; set; }
+        [Required(ErrorMessage = "Field must be filled")]
         public string Name { get; set; }
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Field must be filled")]
         public DateTime CreatingDate { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "Field must be filled")]
         public int Category { get; set; }
         [DataType(DataType.Time)]
+        [Required(ErrorMessage = "Field must be filled")]
         public TimeSpan TimeOfPrompt { get; set; }
         public List<Action> Actions { get; set; }
         public string Image { get; set; }
