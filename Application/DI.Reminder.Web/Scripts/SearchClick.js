dcf3c2b5e6dd5fc77bb877026e53c1b9ef4b3f69
Darya Ignatova <darya.ignatova.99@mail.ru>
2018-06-15 20:46:17 +0400

    Script of search was modified: ajax added

--- Application/DI.Reminder.Web/Scripts/SearchClick.js
+++ Application/DI.Reminder.Web/Scripts/SearchClick.js
@@ -2,21 +2,24 @@
     $('#search').click(function () {
         var counter = $('input[name=search]:checked').val();
         var val = document.getElementsByClassName('search-input')[0].value;
-        $.getJSON(
-            '/Prompt/GetItemsForSearch',
-            { id: counter, value:val },
-            function (jsonData) {
+        $.ajax({
+            type: "GET",
+            url: '/Prompt/GetItemsForSearch',
+            data: {
+                id: counter, value: val
+            },
+            success: function (jsonData) {
                 if (jsonData.isRedirect) {
                     $("#JSON").text("");
                     $("#JSON").append('<div class="boton1" style="padding-top:20px">There are no any items like you want to find. Sorry<div>');
                 }
-                else
-                {
+                else {
                     $("#JSON").text("");
                     $.each(jsonData, function (i) {
                         $("#JSON").append('<div >' + this.Name + '<a style="float:right; margin-right:600px" href="/Prompt/Details/' + this.ID + '">Details</a> ' + '</div>');
                     });
                 }
-            });
+            }
         });
+    });
 });
