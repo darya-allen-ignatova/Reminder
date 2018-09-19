9347f2360c4bdca97fd8dd79310648992ba41d2a
Darya Ignatova <darya.allen.ignatova@gmail.com>
2018-09-19 16:16:16 +0400

    Unnessesary using removed

--- Service/DI.Reminder.Service/DataContract/AdvertisingItem.cs
+++ Service/DI.Reminder.Service/DataContract/AdvertisingItem.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using System.Runtime.Serialization;
-using System.IO;
-using System.Drawing.Imaging;
-using System.Drawing;
 
 namespace DI.Reminder.Service.DataContract
 {
